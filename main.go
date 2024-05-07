e49629ac74e4f7925858d4606d47cbce9d293967
web3santa <usefulstory35@gmail.com>
2024-05-07 21:24:00 +0900

    add create and get account

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
 func main() {
 
-	server := NewAPIServer(":3000")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":3000", store)
 
 	server.Run()
 
